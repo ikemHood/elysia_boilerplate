bae79b9dec9c8bbbb0e76c15cf7a4da49f5dc59a
Ikem <ikempeter2020@gmail.com>
2024-05-13 07:50:13 +0100

    init bun app

---
+++ src/config.ts
@@ -0,0 +1,27 @@
+import data from '../package.json';
+
+const isTestEnvironment = Bun.env.NODE_ENV === 'test';
+
+export default {
+  app: {
+    env: Bun.env.NODE_ENV,
+    name: data.name,
+    version: data.version,
+    host: Bun.env.TEST_APP_HOST || Bun.env.APP_HOST || 'localhost',
+    port: (isTestEnvironment ? Bun.env.TEST_APP_PORT : Bun.env.APP_PORT) || '3000'
+  },
+  db: {
+    mongodb: Bun.env.MONGO_URI!
+  },
+  auth: {
+    jwt: {
+      secret: Bun.env.JWT_SECRET!,
+      expiresIn: Bun.env.JWT_EXPIRES_IN!
+    }
+  },
+  socials: {
+    website: Bun.env.WEBSITE!,
+    x: Bun.env.X_URL!,
+    tg: Bun.env.TG_URL!
+  }
+};
